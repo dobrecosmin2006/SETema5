540e7c10088ecc4c3142405b1c50912ba2b92bc5
CosminDobre <dobrecosmin2006@gmail.com>
2014-11-10 12:13:02 +0200

    Initial commit

---
+++ src/SETema5/patterns/src/Main.java
@@ -0,0 +1,21 @@
+package SETema5.patterns.src;
+
+/**
+ * Created by Mihai on 11/10/2014.
+ *
+ *
+ * this is my Main class for my homework
+ */
+public class Main {
+
+
+    public static void main(String[] args)
+    {
+
+
+
+
+    }
+
+
+}
